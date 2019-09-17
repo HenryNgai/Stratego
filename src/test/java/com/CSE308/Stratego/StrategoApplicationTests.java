35440fea68c713707eab59278ed2dbd2477e862e
Henry Ngai <henryngai@Henrys-MacBook-Pro.local>
2019-09-17 12:37:04 -0400

    Base code for backend. Spring Boot

---
+++ src/test/java/com/CSE308/Stratego/StrategoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.CSE308.Stratego;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StrategoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
