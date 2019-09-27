82f1e1fb5477ed802b97c15e40dd2bd8ae2851cc
Dhruv Sheth <vrockcm@gmail.com>
2019-09-26 21:11:40 -0400

    Login Fixed

--- src/main/java/com/CSE308/Stratego/controller/StrategoController.java
+++ src/main/java/com/CSE308/Stratego/controller/StrategoController.java
@@ -1,35 +1,67 @@
 package com.CSE308.Stratego.controller;
-import com.CSE308.Stratego.model.Services;
-import com.CSE308.Stratego.model.dao.LoginRepository;
+import com.CSE308.Stratego.model.UserService;
+import com.CSE308.Stratego.model.dao.User;
+import com.CSE308.Stratego.model.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 
 @Controller
 public class StrategoController {
-    @Autowired LoginRepository loginRepository;
-    @Autowired Services services;
 
-    @GetMapping("/")
-    public String root() {
-        return "login";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
     }
 
-    @GetMapping("/user/home")
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "There is already a user registered with the email provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/home")
     public String userIndex() {
-        return "user/home";
+        return "/admin/home";
     }
 
     @GetMapping("/game")
     public String Game() {
-        return "user/game";
+        return "/admin/game";
     }
 
     @GetMapping("/access-denied")
---
+++ src/main/java/com/CSE308/Stratego/model/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.CSE308.Stratego.model.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
