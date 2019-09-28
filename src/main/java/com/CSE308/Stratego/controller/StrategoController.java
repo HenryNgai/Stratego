1e04be5982741cb65e8d5cbdda1b9c331dbf0d01
Basim <basim.chowdhury@stonybrook.edu>
2019-09-28 16:37:11 -0400

    restructured response message and added it to the controller

--- src/main/java/com/CSE308/Stratego/controller/StrategoController.java
+++ src/main/java/com/CSE308/Stratego/controller/StrategoController.java
@@ -86,6 +86,9 @@ public class StrategoController {
         System.out.println(x2);
         System.out.println(y2);
         System.out.println(AI);
+
+        String result = game.makeMove(piece, Integer.parseInt(x1), Integer.parseInt(y1), Integer.parseInt(x2), Integer.parseInt(y2), Boolean.parseBoolean(AI));
+        result = result + " 0 0 0 0";
         return new ResponseEntity<>("result successful result",
                 HttpStatus.OK);
     }
