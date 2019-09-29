79f444094ac1f591b539be940d3661c000e848df
henryngai <Henryngai98@gmail.com>
2019-09-29 17:23:00 -0400

    Created the query statements for sending data back to database

--- src/main/java/com/CSE308/Stratego/model/dao/GameDetailRepository.java
+++ src/main/java/com/CSE308/Stratego/model/dao/GameDetailRepository.java
@@ -1,6 +1,7 @@
 package com.CSE308.Stratego.model.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,12 @@ import java.util.List;
 public interface GameDetailRepository extends JpaRepository<GameDetail, Integer>{
     @Query(value = "SELECT u FROM GameDetail u WHERE u.gameId = :gameId")
     public List<GameDetail> getAllGameDetail(@Param("gameId") int gameId);
+
+    @Modifying
+    @Query(value ="insert into PastGame (gameId, piece, whokilledpiece, team) values(:gameId, :piece, :whokilledpiece, team)", nativeQuery = true)
+    public void storePastGame(@Param("gameId") int gameId,
+                              @Param("piece") String piece,
+                              @Param("whokilledpiece") String whokilledpiece,
+                              @Param("team") String team);
 
 }
