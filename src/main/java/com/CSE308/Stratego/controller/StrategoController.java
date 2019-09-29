174ff113e1ce2d0918fc3ef59d5bc7f4e729c38f
Basim <basim.chowdhury@stonybrook.edu>
2019-09-29 16:00:07 -0400

    auto setup done

--- src/main/java/com/CSE308/Stratego/controller/StrategoController.java
+++ src/main/java/com/CSE308/Stratego/controller/StrategoController.java
@@ -165,9 +165,22 @@ public class StrategoController {
         //Random user move
         //Random AI Move
         //Return string with oldX, oldY, newX, newY, oldAIx, oldAIy, newAIx, newAIy
+
+
         String response = "";
         response = response + game.aiMovePiece(game.getUser());
         response = response + " " + game.aiMovePiece(game.getAi());
+        return response;
+    }
+
+    @GetMapping("/autoSetup")
+    public String autoSetup(){
+        //Random user move
+        //Random AI Move
+        //Return string with oldX, oldY, newX, newY, oldAIx, oldAIy, newAIx, newAIy
+
+
+        String response = game.autoSetup();
         return response;
     }
 
